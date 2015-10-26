c915dc0bc5d99b8ba67510c2520c1809b1d2a69e
Yiran Sheng <shengyiran@gmail.com>
2015-10-26 01:34:47 -0700

    updates

---
+++ excercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
@@ -0,0 +1,13 @@
+SELECT hospitals.PROVIDER_ID, hospitals.NAME, best.AVG_SCORE, surveys_hospital.AVG_SURVEY_SCORE
+  FROM hospitals 
+    JOIN (
+      SELECT PROVIDER_ID, AVG(SCORE) AS AVG_SCORE FROM procedures
+        WHERE SCORE IS NOT NULL
+        GROUP BY PROVIDER_ID
+    ) AS best ON (hospitals.PROVIDER_ID = best.PROVIDER_ID)
+    JOIN (
+      SELECT PROVIDER_ID, AVG(HCAHPS_BASE_SCORE) AS AVG_SURVEY_SCORE FROM surveys
+        WHERE HCAHPS_BASE_SCORE IS NOT NULL
+        GROUP BY PROVIDER_ID
+    ) AS surveys_hospital ON (hospitals.PROVIDER_ID = surveys_hospital.PROVIDER_ID)
+  ORDER BY best.AVG_SCORE;
