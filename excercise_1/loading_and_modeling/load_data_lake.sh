cc40e849147bb736174d4fdb3c8314b7186b214e
Yiran Sheng <shengyiran@gmail.com>
2015-10-25 23:17:23 -0700

    quotes

--- excercise_1/loading_and_modeling/load_data_lake.sh
+++ excercise_1/loading_and_modeling/load_data_lake.sh
@@ -6,11 +6,11 @@ unzip Hospital_Revised_Flatfiles.zip -d tmp
 cd tmp
 
 # hospital data 
-tail -n +2 Hospital\ General\ Information.csv > ../hospitals.csv
+tail -n +2 "Hospital General Information.csv" > ../hospitals.csv
 # procedure data
-tail -n +2 Timely\ and\ Effective\ Care\ -\ Hospital.csv > ../effective_care.csv
-tail -n +2 Readmissions\ and\ Deaths\ -\ Hospital.csv > ../readmissions.csv
-tail -n +2 Measure\ Dates.csv > ../measure_dates.csv
+tail -n +2 "Timely and Effective Care - Hospital.csv" > ../effective_care.csv
+tail -n +2 "Readmissions and Deaths - Hospital.csv" > ../readmissions.csv
+tail -n +2 "Measure Dates.csv" > ../measure_dates.csv
 # survey data
 tail -n +2 hvbp_hcahps_05_28_2015.csv >../surveys_responses.csv
 
